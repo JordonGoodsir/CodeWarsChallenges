e6f5d7ab6b0aeae61741708ed2951e86207b5495
Jordon Goodsir <Jordon.goodsir1@gmail.com>
2024-04-08 14:33:42 +1000

    solution complete

--- objectToUrl.js
+++ objectToUrl.js
@@ -1,52 +1,49 @@
 // Converts a URL Query String into an object map
 function convertQueryToMap(query) {
-
     const ansObj = {}
 
-    const split = query.split('.')
+    query.split('&').forEach((section) => {
+        const split = section.split('.')
 
-    let path = []
+        let path = []
 
-    for (let i = 0; i < (split.length); i++) {
-        // console.error('run')
-        if (split[i].includes('=')) {
+        for (let i = 0; i < (split.length); i++) {
+            if (split[i].includes('=')) {
 
-            const equalsSplit = split[i].split('=')
-            path.push(equalsSplit[0])
+                const equalsSplit = split[i].split('=')
+                path.push(equalsSplit[0])
 
-            path.reduce((o, i, index) => {
-                if (index === (split.length - 1)) {
-                    o[i] = equalsSplit[1]
+                path.reduce((o, i, index) => {
+                    if (index === (split.length - 1)) {
+                        o[i] = decodeURIComponent(equalsSplit[1])
 
-                } else if (!o[i]) o[i] = {}
+                    } else if (!o[i]) o[i] = {}
 
-                return o[i]
-
-                // return obj
-            }, ansObj)
+                    return o[i]
+                }, ansObj)
 
 
 
-            ansObj[path[0]]
+                ansObj[path[0]]
 
 
-        } else {
-            path.push(split[i])
-            // ansObj[split[i]] = {}
-            // path += (i ? '.' : '') + split[i]
+            } else {
+                path.push(split[i])
+            }
         }
-    }
+
+    })
 
     console.error(ansObj)
 
-
-
-
-    // add your code here
+    return ansObj
 }
-convertQueryToMap('user.name.firstname=Bob')
+convertQueryToMap('user.name.firstname=Bob&user.name.lastname=Smith&user.favoritecolor=%20Light%20Blue%20')
+
+// Expected: { a: 'a&b=c?' }, instead got: { a: 'a%26b%3Dc%3F' }
 
 
+console.error(String.fromCharCode('3D'))
 // {
 //     'user': {
 //       'name': {
